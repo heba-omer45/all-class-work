633a575499a47c4263b4db84338f10a30b4b9eaf
heba-omer45 <hebaomer547@gmail.com>
2025-01-10 18:14:53 +0500

    Adding on github

---
+++ arrayObjects/index.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        title: "Flower",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem saepe minima eius dolorem! Aspernatur",
+        price: "Pkr/= 2500"
+    },
+    {
+        title: "Flower",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem saepe minima eius dolorem! Aspernatur",
+        price: "Pkr/= 2500"
+    },
+    {
+        title: "Flower",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem saepe minima eius dolorem! Aspernatur",
+        price: "Pkr/= 2500"
+    },
+    {
+        title: "Flower",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem saepe minima eius dolorem! Aspernatur",
+        price: "Pkr/= 2500"
+    },
+    {
+        title: "Flower",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem saepe minima eius dolorem! Aspernatur",
+        price: "Pkr/= 2500"
+    },
+]
